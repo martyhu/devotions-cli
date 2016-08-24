6cbef61313546cb5b959f0961d5f1e657be0689d
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2016-08-24 13:49:28 +0200

    updates bin-check

--- test.js
+++ test.js
@@ -4,10 +4,7 @@ var binCheck = require('bin-check')
 test('check bin', function (t) {
   t.plan(1)
 
-  binCheck('./cli.js', function (err, works) {
-    if (err) {
-      //
-    }
+  binCheck('./cli.js').then(works => {
     t.ok(works, './cli.js has a zero exit code')
   })
 })

8a4fcab0703d6fba1eb5c6c136eedace2c7d0812
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2016-08-24 13:49:22 +0200

    formatting

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var got = require('got')
 var cache = require('./lib/cache.js')
-var key = '/v2/test/dailyVerse'
-var storage_key = 'devotions-cli';
+var storage_key = 'devotions-cli'
 
 var opt = {
   timeout: 10000,
@@ -22,12 +21,12 @@ module.exports = {
 
       got('www.esvapi.org/v2/rest/dailyVerse', opt)
         .then(res => {
-            cache.set(storage_key, res.body, function (err) {
-              if (err) {
-                return callback(err)
-              }
-              callback(err, res.body)
-            })
+          cache.set(storage_key, res.body, function (err) {
+            if (err) {
+              return callback(err)
+            }
+            callback(err, res.body)
+          })
         })
         .catch(err => {
           return callback(err)
